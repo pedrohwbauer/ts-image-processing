cdb48a250e0406b78009c8503cd21e40d262c1ca
pedrohwbauer <pedrohwbauer@gmail.com>
2022-12-03 23:20:46 -0300

    Fix types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -2,11 +2,11 @@ type X = number;
 type Y = number;
 export type PixelPositionMatrix = [X[], Y[], 1[]];
 
-import { CanvasController as CanvasControllerClass } from "@canvas";
+import { CanvasController as CanvasControllerClass } from "@controllers/canvas-controller";
 export type CanvasController = CanvasControllerClass;
 
 import { Transform as TransformAbstract } from "@transform";
 export type Transform = TransformAbstract;
 
-import { createTransforms } from "@factory/transforms-factory";
+import { createTransforms } from "@factories/transforms-factory";
 export type Transforms = ReturnType<typeof createTransforms>;
